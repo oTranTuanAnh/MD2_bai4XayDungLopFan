ff9d55a4ea99da5d8a25a30d0ebbb1e40d01a475
tuan anh <tuananhtran.xd@gmail.com>
2023-12-12 16:40:59 +0700

    first commit

---
+++ Fan.java
@@ -0,0 +1,74 @@
+public class Fan {
+    final static int SLOW = 1;
+    final static int MEDIUM = 2;
+    final static int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public String toString() {
+        if (this.isOn()) {
+            return "Fan is on {" +
+                    "speed=" + speed +
+                    ", on=" + on +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        } else {
+            return "Fan is off {" +
+                    "radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(Fan.FAST,true,6,"red");
+        Fan fan2 = new Fan(Fan.MEDIUM,false,2,"orange");
+        System.out.println(fan1);
+        System.out.println(fan2);
+
+    }
+}
